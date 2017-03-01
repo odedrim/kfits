8245a7d7e9da5ac463125f54d29d12915d099456
Oded Rimon <odedrim@gmail.com>
2017-03-01 20:18:55 +0200

    Executable script 'kfits' added - runs server and opens 127.0.0.1:8000/fitter on web browser

--- afgui/manage.py
+++ afgui/manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-if __name__ == "__main__":
+def main(args):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "afgui.settings")
     try:
         from django.core.management import execute_from_command_line
@@ -19,4 +19,21 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(args)
+
+def run_with_default_settings():
+    import inspect
+    import subprocess
+    import webbrowser
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    # THIS WAY DOESN'T WORK FOR SOME REASON
+    #os.chdir(currentdir)
+    #sys.path.insert(0, currentdir)
+    #main(['manage.py', 'runserver'])
+    # THIS DOES...
+    subprocess.Popen(['python', 'manage.py', 'runserver'], cwd=currentdir)
+    webbrowser.open_new_tab('http://127.0.0.1:8000/fitter')
+
+
+if __name__ == "__main__":
+    main(sys.argv)
